baf759a6458a13b100fa6f5c7bd3075ae669dbb5
Rodrigo <rodrigovf2102@gmail.com>
2022-10-13 10:52:49 -0300

    feat: redirectToUrl controller implemented

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,44 +1,63 @@
 import connection from '../db.js';
 import { StatusCodes } from 'http-status-codes';
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 
-async function urlShorten(req,res){
-    const {url} = req.body;
-    const {userId} = res.locals;
+async function urlShorten(req, res) {
+    const { url } = req.body;
+    const { userId } = res.locals;
     const shortUrl = nanoid(8);
     try {
 
         await connection.query
-        ('INSERT INTO "URLs" (url,"shortUrl","userId","visitCount") VALUES ($1,$2,$3,$4)',
-        [url,shortUrl,userId,0]);
+            ('INSERT INTO "URLs" (url,"shortUrl","userId","visitCount") VALUES ($1,$2,$3,$4)',
+                [url, shortUrl, userId, 0]);
 
         const urlId = (await connection.query('SELECT id FROM "URLs" WHERE "shortUrl"=$1',
-        [shortUrl])).rows[0].id
+            [shortUrl])).rows[0].id
 
         await connection.query('INSERT INTO "userUrls" ("userId","urlId") VALUES ($1,$2)',
-        [userId,urlId]);
+            [userId, urlId]);
 
-        return res.status(StatusCodes.CREATED).send({shortUrl:shortUrl})
+        return res.status(StatusCodes.CREATED).send({ shortUrl: shortUrl })
     } catch (error) {
         console.log(error.message);
         return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
 
-async function getUrls(req,res){
-    const {id} = req.params;
+async function getUrls(req, res) {
+    const { id } = req.params;
     const urlId = Number(id);
-    if(isNaN(urlId) || urlId%1!==0){
+    if (isNaN(urlId) || urlId % 1 !== 0) {
         return res.status(StatusCodes.NOT_FOUND).send('Error: url identification is a integer number');
     }
-    const url = (await connection.query('SELECT * FROM "URLs" WHERE id=$1',[urlId])).rows[0];
-    if(!url){
-        return res.status(StatusCodes.NOT_FOUND).send('Error: url id not found');
+    try {
+        const url = (await connection.query('SELECT * FROM "URLs" WHERE id=$1', [urlId])).rows[0];
+        if (!url) {
+            return res.status(StatusCodes.NOT_FOUND).send('Error: url id not found');
+        }
+        delete url.userId;
+        delete url.visitCount;
+        return res.status(StatusCodes.OK).send(url);
+    } catch (error) {
+        console.log(error.message);
+        return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
     }
-    delete url.userId;
-    delete url.visitCount;
-    return res.status(StatusCodes.ACCEPTED).send(url);
-
 }
 
-export {urlShorten, getUrls}
+async function redirectToUrl(req, res) {
+    const { shortUrl } = req.params;
+    try {
+        const url = (await connection.query
+            ('SELECT url FROM "URLs" WHERE "shortUrl"=$1',[shortUrl])).rows[0].url;
+        if(!url){
+            return res.status(StatusCodes.NOT_FOUND).send('Error: url not found');
+        }
+        return res.redirect(url);
+    } catch (error) {
+        console.log(error.message);
+        return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);       
+    }
+}
+
+export { urlShorten, getUrls, redirectToUrl }
--- src/routers/urls.routes.js
+++ src/routers/urls.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authorization } from '../middlewares/authorization.middlewares.js';
-import { urlShorten, getUrls } from '../controllers/urls.controllers.js';
+import { urlShorten, getUrls, redirectToUrl } from '../controllers/urls.controllers.js';
 import { urlShortenValidation } from '../middlewares/urls.middlewares.js';
 
 const urlsRouter = express.Router();
 
 urlsRouter.post('/urls/shorten',authorization,urlShortenValidation,urlShorten);
 urlsRouter.get('/urls/:id',getUrls);
+urlsRouter.get('/urls/open/:shortUrl',redirectToUrl)
 
 export default urlsRouter;
