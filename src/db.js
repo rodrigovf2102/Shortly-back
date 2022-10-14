d2058892c18fc2e85684b2b0b47b98e9ec9cfdab
Rodrigo <rodrigovf2102@gmail.com>
2022-10-14 10:11:34 -0300

    feat: files prepared for deploy

--- src/db.js
+++ src/db.js
@@ -5,12 +5,21 @@ dotenv.config()
 
 const {Pool} = pg;
 
-const connection = new Pool({
+/*const connection = new Pool({
     user: process.env.USER_DB,
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DATABASE_PORT,
     database: process.env.DATABASE
-})
+})*/
+
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig)
 
 export default connection;
