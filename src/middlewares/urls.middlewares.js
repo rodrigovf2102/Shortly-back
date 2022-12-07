b62de6d9aedd4d3ecf6f5cecf810d5534c9859f3
Rodrigo Vieira Fonseca <rodrigovf2102@gmail.com>
2022-12-07 12:47:24 -0300

    Update urls.middlewares.js

--- src/middlewares/urls.middlewares.js
+++ src/middlewares/urls.middlewares.js
@@ -16,15 +16,6 @@ async function urlShortenValidation(req, res, next) {
         const error = urlValidation.error.details[0].message;
         return res.status(StatusCodes.UNPROCESSABLE_ENTITY).send(error);
     }
-   /* try {
-        const userUrl = (await connection.query('SELECT * FROM "URLs" WHERE url=$1',[url])).rows[0];
-        if(userUrl){
-            return res.status(StatusCodes.CONFLICT).send('Error: url already shrinked by the user');
-        }
-    } catch (error) {
-        console.log(error.message);
-        return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-    }*/
     next();
 }
 
